4f0cc44dd076ecd5c247ab9ad81a8d7695f2efcb
ellnka <alenkasmail@gmail.com>
2017-03-10 10:42:32 +0200

    Update ClockAngel.java

--- src/homeworks/lecture2/ClockAngel.java
+++ src/homeworks/lecture2/ClockAngel.java
@@ -8,7 +8,7 @@ public class ClockAngel {
         float result = 0;
 
         if(hours >= 0 && hours <= 12 && mins >= 0 && mins <= 60) {
-            result = Math.abs((360.0f / 12.0f) * hours + (360.0f / 12.0f / 60.0f * mins) - (360.0f / 60.0f) * mins);
+            result = Math.abs((360.00f / 12.00f) * hours + (360.00f / 12.00f / 60.00f * mins) - (360.00f / 60.00f) * mins);
             result = result > 180 ? 360 - result : result;
             System.out.println("Angle between hours and minute narrows is " + result);
         } else {
