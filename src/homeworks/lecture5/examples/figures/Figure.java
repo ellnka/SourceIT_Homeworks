546b056aafd7d490e4f02d0885451cd4261a1c71
ellnka <alenkasmail@gmail.com>
2017-03-24 21:56:18 +0200

    max count of visitors at once fix

--- src/homeworks/lecture5/examples/figures/Figure.java
+++
@@ -1,6 +0,0 @@
-package edu.source.it.lectures.lecture5.examples.figures;
-
-public interface Figure {
-    double getSquare();
-    void introduce();
-}
--- src/homeworks/lecture5/examples/figures/NewFigure.java
+++
@@ -1,4 +0,0 @@
-package edu.source.it.lectures.lecture5.examples.figures;
-
-public interface NewFigure extends Figure{
-}
